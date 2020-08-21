4038a6467c496f018e346c4d7ba2a600f371384d
tobacco <1353416451@qq.com>
2020-08-21 09:26:00 +0800

    debug

--- src/components/TabBar/index.vue
+++ src/components/TabBar/index.vue
@@ -22,7 +22,7 @@ export default {
 }
 </script>
 <style scoped>
-#footer{ width:100%; height:50px; background: white; border-top:2px #ebe8e3 solid; position: fixed; left: 0; bottom:0;}
+#footer{ width:100%; height:50px; background: white; border-top:2px #ebe8e3 solid; }
 #footer ul{ display: flex; text-align: center; height:50px; align-items:center;}
 #footer ul li{ flex:1; height:40px;}
 #footer ul li.active{ color: #f03d37;}
